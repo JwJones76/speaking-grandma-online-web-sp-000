aeca6fdce5a182d4b4623e1525530f8f1160c6f0
Jacob W Jones <jacobwadejones76@gmail.com>
2020-06-10 16:37:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma
   string = gets.chomp
-  if string = "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
     elsif string.upcase
     puts "NO, NOT SINCE 1938!"
